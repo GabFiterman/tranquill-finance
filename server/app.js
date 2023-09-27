f7a3aa6a980e40b30458a3fd1d027d8bcb0cda25
Fiterman <gfiterman96@gmail.com>
2023-09-27 12:40:59 -0300

    feat: add simple backend

---
+++ server/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor está ouvindo na porta ${port}`);
+});
+
+// Configuração do servidor Express aqui
+app.get('/', async (req, res) => {
+    try {
+        console.log('CONECTADO!');        
+        res.status(200).json({ message: 'Servidor está funcionando!' });
+    } catch (e) {
+        res.status(500).json({ error: `Ocorreu um erro ao se conectar: ${e}` });
+        console.error('Erro ao tentar se conectar:', e);
+    }
+});
