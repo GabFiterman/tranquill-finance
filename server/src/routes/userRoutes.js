2ffe157d3f28281e11108ec6698c441659776ac0
Fiterman <gfiterman96@gmail.com>
2023-09-29 19:31:40 -0300

    minor: update comments

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Cria uma nova conta
+// Cria um novo usuário
 router.post('/create', userController.createUser);
-// Busca todas as contas
+// Busca todas os usuários
 router.get('/get-all', userController.getAllUsers);
-// Busca uma conta específica baseado em um _id
+// Busca uma usuário específico baseado em um _id
 router.get('/:id', userController.getOneUser);
-// Faz update em qualquer informação de uma conta específica baseado em um _id
+// Faz update em qualquer informação de uma usuário específico baseado em um _id
 router.put('/update', userController.updateUser);
-// Deleta uma conta baseado em um _id
+// Deleta uma usuário baseado em um _id
 router.delete('/delete', userController.deleteUser);
 
 module.exports = router;
