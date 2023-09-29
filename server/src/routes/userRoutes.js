468f836c8f7b42e91c75e1abcf92c17e9e059ef3
Fiterman <gfiterman96@gmail.com>
2023-09-29 19:28:12 -0300

    feat: add user CRUD

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -4,14 +4,14 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 // Cria uma nova conta
-router.post('/create-user', userController.createUser);
+router.post('/create', userController.createUser);
 // Busca todas as contas
-router.get('/get-all-users', userController.getAllUsers);
+router.get('/get-all', userController.getAllUsers);
 // Busca uma conta específica baseado em um _id
-router.post('/get-user', userController.getSpecificUser);
+router.get('/:id', userController.getOneUser);
 // Faz update em qualquer informação de uma conta específica baseado em um _id
-router.put('/update-user', userController.updateUser);
+router.put('/update', userController.updateUser);
 // Deleta uma conta baseado em um _id
-router.delete('/delete-user', userController.deleteUser);
+router.delete('/delete', userController.deleteUser);
 
 module.exports = router;
