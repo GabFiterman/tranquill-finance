76b91404113f60fcade4a673e4c5eb63d6699ee7
Fiterman <gfiterman96@gmail.com>
2023-10-07 18:30:31 -0300

    feat: add public-product

--- frontend/src/composables/UseApi.js
+++ frontend/src/composables/UseApi.js
@@ -1,53 +1,98 @@
-import useSupabase from 'src/boot/supabase';
-import useAuthUser from './UseAuthUser';
+import useSupabase from 'src/boot/supabase'
+import useAuthUser from './UseAuthUser'
+import { v4 as uuidv4 } from 'uuid'
 
-export default function useApi() {
-    const { supabase } = useSupabase();
-    const { user } = useAuthUser();
+export default function useApi () {
+  const { supabase } = useSupabase()
+  const { user } = useAuthUser()
 
-    const list = async (table) => {
-        const { data, error } = await supabase.from(table).select('*');
-        if (error) throw error;
-        return data;
-    };
+  const list = async (table) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select('*')
+    if (error) throw error
+    return data
+  }
 
-    const getById = async (table, id) => {
-        const { data, error } = await supabase.from(table).select('*').eq('id', id);
-        if (error) throw error;
-        return data[0];
-    };
+  const listPublic = async (table, userId) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select('*')
+      .eq('user_id', userId)
+    if (error) throw error
+    return data
+  }
 
-    const post = async (table, form) => {
-        const { data, error } = await supabase.from(table).insert([
-            {
-                ...form,
-                user_id: user.value.id,
-            },
-        ]);
-        if (error) throw error;
-        return data;
-    };
+  const getById = async (table, id) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select('*')
+      .eq('id', id)
+    if (error) throw error
+    return data[0]
+  }
 
-    const update = async (table, form) => {
-        const { data, error } = await supabase
-            .from(table)
-            .update([...form])
-            .match({ id: form.id });
-        if (error) throw error;
-        return data;
-    };
+  const post = async (table, form) => {
+    const { data, error } = await supabase
+      .from(table)
+      .insert([
+        {
+          ...form,
+          user_id: user.value.id
+        }
+      ])
+    if (error) throw error
+    return data
+  }
 
-    const remove = async (table, id) => {
-        const { data, error } = await supabase.from(table).delete().match({ id });
-        if (error) throw error;
-        return data;
-    };
+  const update = async (table, form) => {
+    const { data, error } = await supabase
+      .from(table)
+      .update({ ...form })
+      .match({ id: form.id })
+    if (error) throw error
+    return data
+  }
 
-    return {
-        list,
-        getById,
-        post,
-        update,
-        remove,
-    };
+  const remove = async (table, id) => {
+    const { data, error } = await supabase
+      .from(table)
+      .delete()
+      .match({ id })
+    if (error) throw error
+    return data
+  }
+
+  const uploadImg = async (file, storage) => {
+    const fileName = uuidv4()
+    const { error } = supabase
+      .storage
+      .from(storage)
+      .upload(fileName, file, {
+        cacheControl: '3600',
+        upsert: false
+      })
+    const publicUrl = await getUrlPublic(fileName, storage)
+    if (error) throw error
+    return publicUrl
+  }
+
+  const getUrlPublic = async (fileName, storage) => {
+    const { publicURL, error } = supabase
+      .storage
+      .from(storage)
+      .getPublicUrl(fileName)
+    if (error) throw error
+    return publicURL
+  }
+
+  return {
+    list,
+    listPublic,
+    getById,
+    post,
+    update,
+    remove,
+    uploadImg
+  }
 }
