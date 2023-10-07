8dc8b61f9487c48b87ae0a9778e9d5ba060b7c24
Fiterman <gfiterman96@gmail.com>
2023-10-07 18:27:33 -0300

    feat: create form and llist product

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,95 +1,123 @@
 <template>
-    <q-layout view="lHh Lpr lFf">
-        <q-header elevated>
-            <q-toolbar>
-                <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
 
-                <q-toolbar-title> Tranquil Finance </q-toolbar-title>
+        <q-toolbar-title>
+          Estoque
+        </q-toolbar-title>
 
-                <div>Quasar v{{ $q.version }}</div>
-            </q-toolbar>
-        </q-header>
+        <q-btn-dropdown flat color="white" icon="person">
+          <q-list>
+            <q-item clickable v-close-popup @click="handleLogout">
+              <q-item-section>
+                <q-item-label>Logout</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-toolbar>
+    </q-header>
 
-        <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-            <q-list>
-                <q-item-label header> Essential Links </q-item-label>
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Menu
+        </q-item-label>
 
-                <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
-            </q-list>
-        </q-drawer>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
 
-        <q-page-container>
-            <router-view />
-        </q-page-container>
-    </q-layout>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
+import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
-    {
-        title: 'Home',
-        caption: '',
-        icon: 'mdi-home',
-        routeName: 'me',
-    },
-    {
-        title: 'Github',
-        caption: 'github.com/quasarframework',
-        icon: 'code',
-        link: 'https://github.com/quasarframework',
-    },
-    {
-        title: 'Discord Chat Channel',
-        caption: 'chat.quasar.dev',
-        icon: 'chat',
-        link: 'https://chat.quasar.dev',
-    },
-    {
-        title: 'Forum',
-        caption: 'forum.quasar.dev',
-        icon: 'record_voice_over',
-        link: 'https://forum.quasar.dev',
-    },
-    {
-        title: 'Twitter',
-        caption: '@quasarframework',
-        icon: 'rss_feed',
-        link: 'https://twitter.quasar.dev',
-    },
-    {
-        title: 'Facebook',
-        caption: '@QuasarFramework',
-        icon: 'public',
-        link: 'https://facebook.quasar.dev',
-    },
-    {
-        title: 'Quasar Awesome',
-        caption: 'Community Quasar projects',
-        icon: 'favorite',
-        link: 'https://awesome.quasar.dev',
-    },
-];
+  {
+    title: 'Home',
+    caption: '',
+    icon: 'mdi-home',
+    routeName: 'me'
+  },
+  {
+    title: 'Category',
+    caption: '',
+    icon: 'mdi-shape-outline',
+    routeName: 'category'
+  },
+  {
+    title: 'Product',
+    caption: '',
+    icon: 'mdi-archive',
+    routeName: 'product'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+import useAuthUser from 'src/composables/UseAuthUser'
+import { useRouter } from 'vue-router'
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
-    name: 'MainLayout',
+  name: 'MainLayout',
 
-    components: {
-        EssentialLink,
-    },
+  components: {
+    EssentialLink
+  },
 
-    setup() {
-        const leftDrawerOpen = ref(false);
+  setup () {
+    const leftDrawerOpen = ref(false)
 
-        return {
-            essentialLinks: linksList,
-            leftDrawerOpen,
-            toggleLeftDrawer() {
-                leftDrawerOpen.value = !leftDrawerOpen.value;
-            },
-        };
-    },
-});
+    const $q = useQuasar()
+
+    const router = useRouter()
+
+    const { logout } = useAuthUser()
+
+    const handleLogout = async () => {
+      $q.dialog({
+        title: 'Logout',
+        message: 'Do you really want to leave ?',
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        await logout()
+        router.replace({ name: 'login' })
+      })
+    }
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+      handleLogout
+    }
+  }
+})
 </script>
