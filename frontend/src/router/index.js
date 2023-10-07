122ec431257ac7730e2bce673a0d766fe55ef23f
Fiterman <gfiterman96@gmail.com>
2023-10-07 16:07:27 -0300

    feat: add User Auth methods

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,8 @@ import {
     createWebHashHistory,
 } from 'vue-router';
 import routes from './routes';
+// import useAuthUser from '../composables/UseAuthUser'
+import useAuthUser from 'src/composables/useAuthUser';
 
 /*
  * If not building with SSR mode, you can
@@ -32,6 +34,26 @@ export default route(function (/* { store, ssrContext } */) {
         // quasar.conf.js -> build -> publicPath
         history: createHistory(process.env.VUE_ROUTER_BASE),
     });
+
+    Router.beforeEach((to) => {
+      const { isLoggedIn } = useAuthUser()
+
+      if (
+        to.hash.includes('type=recovery') &&
+        to.name !== 'reset-password'
+      ) {
+        const accessToken = to.hash.split('&')[0]
+        const token = accessToken.replace('#access_token=', '')
+        return { name: 'reset-password', query: { token } }
+      }
+      if (
+        !isLoggedIn() &&
+        to.meta.requiresAuth &&
+        !Object.keys(to.query).includes('fromEmail')
+      ) {
+        return { name: 'login' }
+      }
+    })
 
     return Router;
 });
