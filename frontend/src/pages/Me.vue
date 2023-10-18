725ae2a57e8bd8fe0f7fed2675e6613b33a44d1e
Fiterman <gfiterman96@gmail.com>
2023-10-18 00:46:02 -0300

    feat: show account cards at home page

--- frontend/src/pages/Me.vue
+++ frontend/src/pages/Me.vue
@@ -2,21 +2,58 @@
     <q-page class="flex flex-center">
         <div v-if="user">
             <div>Hello</div>
+            <template v-if="allAccounts">
+                <div class="q-pa-md">
+                    <q-carousel animated v-model="slide" arrows navigation infinite>
+                        <q-carousel-slide
+                            v-for="(account, index) in allAccounts"
+                            :key="account.id"
+                            :name="index"
+                        >
+                        <CardAccount :account="account" />
+                      </q-carousel-slide>
+                    </q-carousel>
+                </div>
+            </template>
         </div>
     </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 import useAuthUser from 'src/composables/UseAuthUser';
+import CardAccount from 'src/components/CardAccount.vue';
+import { api } from 'src/boot/axios';
 
 export default defineComponent({
     name: 'PageMe',
+    components: {
+        CardAccount,
+    },
     setup() {
         const { user } = useAuthUser();
-        
+        const allAccounts = ref([]);
+
+        onMounted(() => {
+            getAccounts();
+        });
+
+        const getAccounts = async () => {
+            await api
+                .get('/account/get-all')
+                .then((response) => {
+                    allAccounts.value = response.data.data;
+                    console.log('Response: ', response.data.data);
+                })
+                .catch((error) => {
+                    console.log('Error: ', error);
+                });
+        };
+
         return {
             user,
+            allAccounts,
+            slide: ref(1),
         };
     },
 });
