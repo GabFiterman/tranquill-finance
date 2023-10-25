7ab3185cbb13e4d28724980b136410003ad96b86
Fiterman <gfiterman96@gmail.com>
2023-10-25 12:27:08 -0300

    patch: improving transaction card with account and category fetch - conditional color for revenue input/output - renders account alias

--- frontend/src/components/CardHistoryTransaction.vue
+++ frontend/src/components/CardHistoryTransaction.vue
@@ -2,13 +2,21 @@
     <q-card padding>
         <q-card-section horizontal class="justify-between q-px-md">
             <q-card-section class="flex column justify-between">
+                <!-- TODO: formatar data -->
                 <p class="text-positive">{{ transaction_date }}</p>
                 <p>{{ description }}</p>
-                <p>{{ account }}</p>
+                <p v-if="transactionAccount">{{ transactionAccount?.alias }}</p>
             </q-card-section>
             <q-card-section>
-                <h3>
-                    <span class="text-caption">{{ payment_method }}</span>
+                <span class="text-caption">{{ payment_method }}</span>
+                <h3
+                    :class="
+                        transactionCategory?.category_type === 'Receita'
+                            ? 'text-positive'
+                            : 'text-negative'
+                    "
+                    class="text-bold"
+                >
                     {{ value }}
                 </h3>
             </q-card-section>
@@ -17,7 +25,8 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import { api } from 'src/boot/axios';
 
 export default defineComponent({
     name: 'CardAccount',
@@ -30,13 +39,43 @@ export default defineComponent({
     setup(props) {
         const { value, description, transaction_date, category, account, payment_method } =
             props.transaction;
+        const transactionAccount = ref(null);
+        const transactionCategory = ref(null);
+
+        onMounted(() => {
+            getAccount();
+            getCategory();
+        });
+
+        const getAccount = async () => {
+            await api
+                .get(`/account/${account}`)
+                .then((response) => {
+                    transactionAccount.value = response.data.data[0];
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                });
+        };
+
+        const getCategory = async () => {
+            await api
+                .get(`/category/${category}`)
+                .then((response) => {
+                    transactionCategory.value = response.data.data[0];
+                })
+                .catch((error) => {
+                    throw new Error(error);
+                });
+        };
 
         return {
             value,
             description,
             transaction_date,
             category,
-            account,
+            transactionAccount,
+            transactionCategory,
             payment_method,
         };
     },
