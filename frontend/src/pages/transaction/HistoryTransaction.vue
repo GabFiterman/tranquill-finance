b41f0e2556d0241b9f3f9f92d898afacbbb396f4
Fiterman <gfiterman96@gmail.com>
2023-10-25 11:12:31 -0300

    chore: create history transaction route

---
+++ frontend/src/pages/transaction/HistoryTransaction.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <div class="row">
+      <h3>History transaction</h3>
+    </div>
+    <div class="q-px-md">
+      <!-- entrada / saída -->
+      lista dos cards
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+  },
+})
+</script>
