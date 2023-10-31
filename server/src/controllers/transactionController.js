55a1df6af0794ec0544bbcd283bc44d2dcb5a8d2
Fiterman <gfiterman96@gmail.com>
2023-10-30 21:09:42 -0300

    feat: when create transaction, updates accountBalance

--- server/src/controllers/transactionController.js
+++ server/src/controllers/transactionController.js
@@ -8,8 +8,26 @@ const {
     updateDocument,
     deleteDocument,
 } = require('../common/crud');
+const { updateAccountBalance } = require('../controllers/accountController');
 
-exports.createTransaction = createDocument(transactionModel);
+exports.createTransaction = async (req, res) => {
+    try {
+        const create = createDocument(transactionModel);
+        await create(req, res);
+
+        const send = {
+            body: {
+                _id: req.body.account,
+                value: req.body.value,
+                type: req.body.type,
+            },
+        };
+        await updateAccountBalance(send, res, false);
+    } catch (err) {
+        // TODO: melhorar tratamento de erros
+        throw new Error(err);
+    }
+};
 exports.getAllTransactions = readAllDocuments(transactionModel);
 exports.getAllTransactionsFromUser = readAllDocumentsFromUser(transactionModel);
 exports.getOneTransaction = readOneDocument(transactionModel);
