5b31475fd893ec869fd875cbb7637bc532fd4beb
Fiterman <gfiterman96@gmail.com>
2023-11-04 01:14:18 -0300

    style: ajust emailConfirmation page to neumorphism

--- frontend/src/pages/user/EmailConfirmation.vue
+++ frontend/src/pages/user/EmailConfirmation.vue
@@ -1,17 +1,18 @@
 <template>
-    <q-page padding>
-        <p class="text-body1">Thanks for registering !</p>
-        <p class="text-body1">
-            Please confirm your email to finishing registering:
-            <strong>{{ $route.query.email }}</strong>
-        </p>
+    <q-page padding class="text-center q-mt-xl" style="height: 60vh">
+        <p class="text-body2 opacity-75">obrigado por se registrar</p>
+        <p class="text-body1">falta pouco...</p>
+        <div class="row q-mt-xl q-mb-md">
+            <p class="text-body1 q-mt-xl">
+                por favor confirme seu email para completar seu cadastro: <br /><br />
+                <strong>{{ $route.query.email }}</strong>
+            </p>
+        </div>
         <div class="row justify-center">
             <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-md">
                 <q-btn
-                    label="Back to Login"
-                    color="dark"
-                    class="full-width"
-                    rounded
+                    label="voltar ao login"
+                    class="full-width button nph-out"
                     :to="{ name: 'login-user' }"
                 />
             </div>
